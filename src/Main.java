10f96e382e63d4006b843f3ab3e69e3173e15fb1
mrevecontes <Domas12@yandex.ru>
2023-10-03 13:11:37 +0300

    Добавили проверку числа на четность Добавил начальное сообщение

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Введите целое число: ");
         Scanner scnr = new Scanner(System.in);
         int num = 0;
         if (scnr.hasNextInt()) {
@@ -10,5 +11,5 @@ public class Main {
                 System.out.println("Это целое четное число");
             } else System.out.println("Ошибка! Это не четное число");
         } else System.out.println("Вы ввели не целое число");
-    }○
+    }
 }
